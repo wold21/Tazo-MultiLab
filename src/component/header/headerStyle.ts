df5dedb4bbcb54a4bc13cc9b2cd308090abf8bc7
hyuk.kim <wold2180@gmail.com>
2024-09-07 14:36:33 +0900

    updated: prettier 정리

--- src/component/header/headerStyle.ts
+++ src/component/header/headerStyle.ts
@@ -1,29 +1,29 @@
-import styled from "styled-components";
-import GoldenRetriever from "../../assets/images/goldenRetriever.png";
+import styled from 'styled-components';
+import GoldenRetriever from '../../assets/images/goldenRetriever.png';
 
 export const HeaderBox = styled.header`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    min-width: 1100px;
-    height: 100px;
-    background: linear-gradient(to top, transparent, #222222);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    z-index: 9999;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	min-width: 1100px;
+	height: 100px;
+	background: linear-gradient(to top, transparent, #222222);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	z-index: 9999;
 `;
 
 export const Title = styled.div`
-    width: 320px;
-    min-width: 320px;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0px 20px;
-    cursor: pointer;
+	width: 320px;
+	min-width: 320px;
+	height: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0px 20px;
+	cursor: pointer;
 `;
 
 // export const Help = styled.div`
@@ -37,31 +37,35 @@ export const Title = styled.div`
 //     cursor: pointer;
 // `;
 export const LogoBox = styled.div`
-    width: 70px;
-    height: 70px;
+	width: 70px;
+	height: 70px;
 `;
 export const Logo = styled.div`
-    width: 100%;
-    height: 100%;
-    background-image: url(${GoldenRetriever});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
+	width: 100%;
+	height: 100%;
+	background-image: url(${GoldenRetriever});
+	background-size: contain;
+	background-repeat: no-repeat;
+	background-position: center;
 `;
 export const TitleText = styled.div`
-    color: white;
-    user-select: none;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    gap: 10px 0;
+	color: white;
+	user-select: none;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+	gap: 10px 0;
 `;
 export const MainText = styled.h1`
-    font-size: 2rem;
+	font-size: 2rem;
 `;
 export const SubText = styled.h2`
-    font-size: 0.8rem;
-    color: #696969;
-    text-shadow: -1px 0px #000000, 0px 1px #000000, 0px -1px #000000, 1px 0px #000000;
+	font-size: 0.8rem;
+	color: #696969;
+	text-shadow:
+		-1px 0px #000000,
+		0px 1px #000000,
+		0px -1px #000000,
+		1px 0px #000000;
 `;
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -8,30 +8,35 @@ export const GlobalStyle = createGlobalStyle`
     src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2403-2@1.0/TTLaundryGothicB.woff2') format('woff2');
     font-weight: 700;
     font-style: normal;
-}
+  }
+  @font-face {
+    font-family: 'Suit';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
   a{
     text-decoration: none;
     color: inherit;
-}
-*{
+  }
+  *{
     box-sizing: border-box;
-}
-html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-a, ol, ul, li, form, label{
+  }
+  html, body, div, span, h1, h2, h3, h4, h5, h6, p, a, ol, ul, li, form, label{
     margin: 0;
     padding: 0;
     border: 0;
     vertical-align: baseline;
-}
-body{
+  }
+  body{
     font-family: LaundryGothic, 'Noto Sans KR', sans-serif;
     overflow-x: hidden;
     height: 100vh;
-}
-ol, ul{
+  }
+  ol, ul{
     list-style: none;
-}
-    ::-webkit-scrollbar {
+  }
+  ::-webkit-scrollbar {
     width: 8px;
   }
   ::-webkit-scrollbar-thumb {
@@ -40,10 +45,10 @@ ol, ul{
   ::-webkit-scrollbar-thumb:hover {
     background-color: #222222;
   }
-    ::-webkit-scrollbar-track {
+  ::-webkit-scrollbar-track {
     background-color: #222222;
-}
-     #root {
+  }
+  #root {
     display: flex;
     height: 100vh;
     flex-direction: column;
